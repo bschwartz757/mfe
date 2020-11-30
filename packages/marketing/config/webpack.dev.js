6a8661daeb5937ec6d8ec2b6928ce765c3818796
Blake <blakehschwartz@gmail.com>
2020-11-30 15:09:09 -0800

    Removes html-webpack-plugin from marketing/shared webpack config

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,4 +1,5 @@
 const { merge } = require('webpack-merge')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 const commonConfig = require('./webpack.common')
 const pJson = require('../package.json')
@@ -19,6 +20,9 @@ const devConfig = {
         './MarketingApp': './src/bootstrap',
       },
       shared: pJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
     }),
   ],
 }
